9699804cc3825c11727a2cc9c8e9901061032d3f
RSvegin <robinsvegin@gmail.com>
2026-01-28 08:38:14 +0000

    achieved sanity

--- Part-i.py
+++ Part-i.py
@@ -2,8 +2,10 @@ import Radial_functions as Rf
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 r = np.linspace(0,20,1000) 
 Z = 1
 P = Rf.P1s(r,Z)
 plt.plot(r,P)
 plt.show
+plt.savefig(P1s.png)
